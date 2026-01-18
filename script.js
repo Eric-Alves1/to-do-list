d851daed1b2e74da0c89d9df39f4d70017ba90bd
Eric-Alves1 <ericalves.429634@gmail.com>
2026-01-18 16:29:22 -0300

    finishing functuon addItem

--- script.js
+++ script.js
@@ -10,38 +10,60 @@ function clearInput() {
 
 function addItem() {
   let value = input.value;
-  todos.push(value);
-  if (todos.length > 5) {
+  if (value === "") {
+    alert('you must write something!')
+    return;
+  }
+  
+  if (todos.length >= 5) {
     alert('you can only write 5 todos!');
     clearInput();
     return;
   }
+  todos.push(value);
   
-  if (value === "") {
-    alert('you must write something!')
-  } else {
-    const todoItem = document.createElement('h3');
-    todoItem.classList.add('todo-item');
-    todoItem.textContent = value;
-    document.body.appendChild(todoItem);
+  const todoContainer = document.createElement('div');
+  todoContainer.classList.add('todo')
+  
+  const todoItem = document.createElement('h3');
+  todoItem.classList.add('todo-item');
+  todoItem.textContent = value;
     
-    const buttonEdit = document.createElement('button');
-    buttonEdit.classList.add('button-edit')
-    buttonEdit.textContent = 'edit';
-    document.body.appendChild(buttonEdit);
+  const buttonEdit = document.createElement('button');
+  buttonEdit.classList.add('button-edit')
+  buttonEdit.textContent = 'edit';
+  buttonEdit.addEventListener('click', btnEdit);
     
-    const buttonDelete = document.createElement('button');
-    buttonDelete.classList.add('button-delete')
-    buttonDelete.textContent = 'delete';
-    document.body.appendChild(buttonDelete);
+  const buttonDelete = document.createElement('button');
+  buttonDelete.classList.add('button-delete')
+  buttonDelete.textContent = 'delete';
+  buttonDelete.addEventListener('click', btnDelete);
     
-    const buttonFinish = document.createElement('button');
-    buttonFinish.classList.add('button-finish');
-    buttonFinish.textContent = 'finish';
-    document.body.appendChild(buttonFinish);
+  const buttonFinish = document.createElement('button');
+  buttonFinish.classList.add('button-finish');
+  buttonFinish.textContent = 'finish';
+  buttonFinish.addEventListener('click', btnFinish);
     
-    clearInput();
-  }
+  todoContainer.appendChild(todoItem)
+  todoContainer.appendChild(buttonEdit)
+  todoContainer.appendChild(buttonDelete)
+  todoContainer.appendChild(buttonFinish)
+  
+  todoList.appendChild(todoContainer);
+  
+  clearInput();
+}
+
+function btnEdit() {
+  alert('its working');
+}
+
+function btnDelete() {
+  alert('its working');
+}
+
+function btnFinish() {
+  alert('its working');
 }
 
 buttonAdd.addEventListener('click', addItem);
